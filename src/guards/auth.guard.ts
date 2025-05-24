b9e6d24b7736eb62c285c6a42b9cf34a7a677edb
Ilya Uhanev <ilya.uhanev@gmail.com>
2025-05-24 20:11:13 +0500

    Added auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import { JwtService } from "@nestjs/jwt";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Request } from "express";
+import { ConfigService } from "@nestjs/config";
+
+export interface AuthRequest {
+  cookies: {
+    auth?: string;
+  };
+}
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: AuthRequest = context.switchToHttp().getRequest();
+    const token = request.cookies.auth;
+    if (!token) throw new UnauthorizedException();
+
+    try {
+      await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get("JWT_SECRET_KEY"),
+      });
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
