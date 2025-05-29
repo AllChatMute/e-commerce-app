b736366ef80cd8f2207dc7b3c14a090ca0b85762
Ilya Uhanev <ilya.uhanev@gmail.com>
2025-05-29 16:54:23 +0500

    products service tests done

--- src/modules/products/products.service.spec.ts
+++ src/modules/products/products.service.spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { ProductsService } from "./products.service";
 import { getModelToken } from "@nestjs/mongoose";
 import { CreateProductDto } from "./types/createProductDto";
+import { NotFoundException } from "@nestjs/common";
+import { Model } from "mongoose";
+import { Product } from "../../schemas/product.schema";
 
 const createProduct: CreateProductDto = {
   name: "mockName",
@@ -9,39 +12,39 @@ const createProduct: CreateProductDto = {
   price: 3000,
   categories: ["mock", "test"],
 };
+
 const returnedProduct = {
   productId: 1,
-  name: "mockName",
-  description: "mockDescription",
-  price: 3000,
-  categories: ["mock", "test"],
+  ...createProduct,
 };
+
+const mockFunc = jest.fn().mockResolvedValue(returnedProduct);
 
 describe("ProductsService", () => {
   let service: ProductsService;
+  let model: Model<Product>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        {
-          provide: ProductsService,
-          useValue: {
-            createProduct: jest.fn().mockResolvedValue(returnedProduct),
-          },
-        },
+        ProductsService,
+
         {
           provide: getModelToken("Product"),
           useValue: {
-            findOne: jest.fn().mockResolvedValue(returnedProduct),
-            insertOne: jest.fn().mockResolvedValue(returnedProduct),
-            findOneAndUpdate: jest.fn().mockResolvedValue(returnedProduct),
-            findOneAndDelete: jest.fn().mockResolvedValue(returnedProduct),
+            findOne: mockFunc,
+            insertOne: mockFunc,
+            findOneAndUpdate: mockFunc,
+            findOneAndDelete: mockFunc,
+            find: jest.fn().mockResolvedValue([]),
+            aggregate: jest.fn().mockResolvedValue([returnedProduct]),
           },
         },
       ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    model = module.get<Model<Product>>(getModelToken("Product"));
   });
 
   it("should be defined", () => {
@@ -59,5 +62,45 @@ describe("ProductsService", () => {
         price: expect.any(Number) as number,
       })
     );
+  });
+
+  it("should return updated product", async () => {
+    const response = await service.updateProduct(1, createProduct);
+    expect(response).toEqual(returnedProduct);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        productId: expect.any(Number) as number,
+        name: expect.any(String) as string,
+        description: expect.any(String) as string,
+        price: expect.any(Number) as number,
+      })
+    );
+  });
+
+  it("should return single product", async () => {
+    const response = await service.getProductById(1);
+
+    expect(response).toEqual(returnedProduct);
+    expect(response).toEqual(
+      expect.objectContaining({
+        productId: expect.any(Number) as number,
+        name: expect.any(String) as string,
+        description: expect.any(String) as string,
+        price: expect.any(Number) as number,
+      })
+    );
+  });
+
+  it("should return an array of products", async () => {
+    const response = await service.getProducts("mockName", "mock,test");
+
+    expect(response).toEqual([returnedProduct]);
+  });
+
+  it("should throw 404 if not found", async () => {
+    jest.spyOn(model, "findOne").mockResolvedValue(null);
+
+    await expect(service.getProductById(1)).rejects.toThrow(NotFoundException);
   });
 });
