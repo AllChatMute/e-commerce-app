5cedefc26537e5473b2b6d43c551c27304d338f6
Ilya Uhanev <ilya.uhanev@gmail.com>
2025-06-23 23:13:28 +0500

    Updated Payment schema

--- src/schemas/payment.schema.ts
+++ src/schemas/payment.schema.ts
@@ -1,18 +1,25 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
+import { currency } from "../types/currency.enum";
 
 export type PaymentDocument = HydratedDocument<Payment>;
 
 @Schema()
 export class Payment {
   @Prop({ required: true })
-  email: string;
+  id: number;
 
   @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true, min: 1 })
   amount: number;
 
   @Prop({ required: true })
-  currency: string;
+  currency: currency;
+
+  @Prop({ required: true })
+  status: string;
 }
 
 export const PaymentSchema = SchemaFactory.createForClass(Payment);
