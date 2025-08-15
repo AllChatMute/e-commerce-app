73208b68857fe3af6f47e931f42ea2d786766ab0
Ilya Uhanev <ilya.uhanev@gmail.com>
2025-08-15 21:40:00 +0500

    Added dependencies mocks for payment controller and service unit tests

--- src/modules/payments/payments.controller.spec.ts
+++ src/modules/payments/payments.controller.spec.ts
@@ -1,6 +1,22 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PaymentsController } from "./payments.controller";
 import { PaymentsService } from "./payments.service";
+import { PaymentsRepositoryService } from "../../services/paymentRepository.service";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { getModelToken } from "@nestjs/mongoose";
+import { RequestWithEmail } from "../../types/requestWithEmail.interface";
+import { CreatePaymentDto } from "./types/createPaymentDto";
+import { Currency } from "../../types/currency.enum";
+
+const mockRequest = {
+  email: "mock@mail.com",
+} as unknown as RequestWithEmail;
+
+const mockCreatePaymentDto: CreatePaymentDto = {
+  amount: 1000,
+  currency: Currency.RUB,
+};
 
 describe("PaymentsController", () => {
   let controller: PaymentsController;
@@ -8,7 +24,13 @@ describe("PaymentsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentsController],
-      providers: [PaymentsService],
+      providers: [
+        { provide: PaymentsService, useValue: {} },
+        PaymentsRepositoryService,
+        { provide: getModelToken("Payment"), useValue: {} },
+        JwtService,
+        ConfigService,
+      ],
     }).compile();
 
     controller = module.get<PaymentsController>(PaymentsController);
@@ -16,5 +38,9 @@ describe("PaymentsController", () => {
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("should return created payment ", async () => {
+    expect(await controller.createPayment(mockCreatePaymentDto, mockRequest)).;
   });
 });
--- src/modules/payments/payments.service.spec.ts
+++ src/modules/payments/payments.service.spec.ts
@@ -1,18 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PaymentsService } from './payments.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { PaymentsService } from "./payments.service";
+import { PaymentsRepositoryService } from "../../services/paymentRepository.service";
+import { getModelToken } from "@nestjs/mongoose";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
-describe('PaymentsService', () => {
+describe("PaymentsService", () => {
   let service: PaymentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentsService],
+      providers: [
+        PaymentsService,
+        PaymentsRepositoryService,
+        { provide: getModelToken("Payment"), useValue: {} },
+        JwtService,
+        ConfigService,
+      ],
     }).compile();
 
     service = module.get<PaymentsService>(PaymentsService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
